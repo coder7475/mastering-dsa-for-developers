9c508f92045bdb731021fdb3a2d6ff4e0b9e965a
Hasan Mahmud Nayem <hasan.mnayem@toptal.com>
2025-04-18 02:59:19 +0600

    Initial commit - setup necessary projects

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<div className='container'>
+			<div className='flex flex-col items-center justify-center h-screen'>
+				<h1 className='text-4xl font-bold'>Hello World</h1>
+				<button className='bg-blue-500 text-white my-2 py-2 px-4 rounded-md'>
+					Click me
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default App;
