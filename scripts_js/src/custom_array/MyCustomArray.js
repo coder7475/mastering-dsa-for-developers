72f222ae7100fd878efb44e53810dc8ed9e012c2
coder7475 <robiulhossain7475@gmail.com>
2025-04-24 21:51:33 +0600

    feat: create array insert method

--- scripts_js/src/custom_array/MyCustomArray.js
+++ scripts_js/src/custom_array/MyCustomArray.js
@@ -6,4 +6,20 @@ class MYCustomArray {
     this.length = 0;
     this.array = new Array(capacity);
   }
+
+  insert(value, index) {
+    if (index < 0 || index > this.length) {
+      throw new Error('Index is out of bounds')
+    }
+    // last index
+    if (index === this.length) {
+      this.array[index] = value;
+      return;
+    }
+
+    for (let i = this.length; i > index; i--) {
+      this.array[i] = this.array[i - 1];
+    }
+
+  }
 }
